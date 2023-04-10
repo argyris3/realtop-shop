de61d0cb09d33769434703d2b9e889b6b611e2b0
Argyris3 <bigol3@hotmail.com>
2023-04-10 09:03:43 +0300

    sign in functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,11 +1,9 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 import { FcGoogle } from 'react-icons/fc';
 import { toast } from 'react-toastify';
-import { getAuth, signInWithPopup } from 'firebase/auth';
-import { GoogleAuthProvider } from 'firebase/auth';
-import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useNavigate } from 'react-router-dom';
-
 export default function OAuth() {
   const navigate = useNavigate();
   async function onGoogleClick() {
@@ -14,7 +12,9 @@ export default function OAuth() {
       const provider = new GoogleAuthProvider();
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
-      //check for the user
+
+      // check for the user
+
       const docRef = doc(db, 'users', user.uid);
       const docSnap = await getDoc(docRef);
 
@@ -25,21 +25,20 @@ export default function OAuth() {
           timestamp: serverTimestamp(),
         });
       }
+
       navigate('/');
     } catch (error) {
       toast.error('Could not authorize with Google');
-      console.log(error);
     }
   }
-
   return (
     <button
       type="button"
       onClick={onGoogleClick}
       className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
     >
-      <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
-      Continue With Google
+      <FcGoogle className="text-2xl  bg-white rounded-full mr-2" />
+      Continue with Google
     </button>
   );
 }
